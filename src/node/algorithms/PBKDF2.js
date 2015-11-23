0e3d6477a5b6919a10bbafc69f190886492f3755
Christian Olbrich <co@secomba.com>
2015-11-23 13:30:54 +0100

    sha512 & pbkdf2

---
+++ src/node/algorithms/PBKDF2.js
@@ -0,0 +1,52 @@
+var pbkdf2 = {
+    sha256: require("pbkdf2-sha256"),
+    sha512: require("pbkdf2-sha512")
+};
+
+var PBKDF2 = {
+    checkParams: function(format, algorithm) {
+        return format === "raw" && algorithm.name === "PBKDF2";
+    },
+
+    usages: {'deriveBits': 'deriveBits'},
+
+    formats: {
+        raw: {
+            types: [
+                {
+                    label: 'deriveBits',
+                    usage: {
+                        deriveBits: function(key, algorithm) {
+                            return function(options, length) {
+                                return PBKDF2.deriveBits(key, options, length);
+                            };
+                        }
+                    }
+                }
+            ],
+
+            import: function(key, algorithm) {
+                return new Buffer(key);
+            }
+        }
+    },
+
+    createExporter(type, key) {
+        throw "not implemented";
+    },
+
+    deriveBits(key, options, length) {
+        var method = {"SHA-512": "sha512", "SHA-256": "sha256"}[options.hash.name];
+
+        key = key.toString('utf8');
+        var salt = new Buffer(options.salt).toString('utf8'); // TODO: make this work without string-encodings
+        var result = pbkdf2[method](key, salt, options.iterations, length / 8);
+        var nativeResult = new Uint8Array(result.length);
+        for (var i = 0; i < result.length; i++) {
+            nativeResult[i] = result[i];
+        }
+        return nativeResult;
+    }
+};
+
+module.exports = PBKDF2;
--- src/node/digest.js
+++ src/node/digest.js
@@ -1,4 +1,5 @@
 var forge = require("node-forge")
+var Hashes = require("jshashes")
 var subtleToForge = {
   "SHA-1" : "sha1",
   "SHA-256" : "sha256",
@@ -9,10 +10,15 @@ var digest = function digest(alg, data){
   if (!subtleToForge[alg.name])
     return Promise.reject("unsupported hashing algorithm")
   else{
-    var md = forge.md[subtleToForge[alg.name]].create()
-    md.update(data.toString("binary"))
+      if (alg.name === "SHA-512") {
+          // special treatment, not covered by this forge version
+          return new Buffer(new Hashes.SHA512().hex(data.toString("binary")), "hex");
+      } else {
+          var md = forge.md[subtleToForge[alg.name]].create()
+          md.update(data.toString("binary"))
 
-    return new Buffer(md.digest().bytes(), "binary")
+          return new Buffer(md.digest().bytes(), "binary")
+      }
   }
 };
 
