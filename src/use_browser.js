a06741a94b410b3234c4c18baf07eac052028515
Christian Olbrich <co@secomba.com>
2015-11-25 11:47:47 +0100

    fixed "always-JS" bug

--- src/use_browser.js
+++ src/use_browser.js
@@ -2,12 +2,13 @@ require("./forge.min.js")
 var importKey = require("./node/importKey")
 
 function tryBrowser (routine, arguments){
+    const tryBrowserArguments = arguments;
     return new Promise(function(resolve, reject) {
-        var result = crypto.subtle[routine].apply(crypto.subtle, arguments);
+        var result = crypto.subtle[routine].apply(crypto.subtle, tryBrowserArguments);
         if (routine === "importKey") {
             // importKey is difficult: The import-operation might succeed, while the the usage of the key
             // might fail - in which case we need the JS-equivalent. Therefore, we're adding this here.
-            return Promise.all([result, importKey.apply(importKey, arguments)])
+            return Promise.all([result, importKey.apply(importKey, tryBrowserArguments)])
             .then(function (results) {
                 resolve({webcryptoKey: result[0], jsKey: result[1]});
             })
