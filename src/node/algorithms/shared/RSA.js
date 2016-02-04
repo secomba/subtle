851a9f9d0292c5294fba0a1d65b58c42e169812a
Christian Olbrich <co@secomba.com>
2016-02-04 15:52:59 +0100

    added public key jwk encryption

--- src/node/algorithms/shared/RSA.js
+++ src/node/algorithms/shared/RSA.js
@@ -1,6 +1,23 @@
+var asn = require('asn1.js');
 var forge = require("node-forge")
   , pkcsPad1  = new Buffer([48, 130])
   , pkcsPad2  = new Buffer([2, 1, 0, 48, 13, 6, 9, 42, 134, 72, 134, 247, 13, 1, 1, 1, 5, 0, 4, 130])
+
+//TODO: move this elsewhere
+function base64urldecode(encodedString) {
+    encodedString = encodedString.replace(/-/g, '+').replace(/_/g, '/');
+    encodedString += ['', '', '==', '='][encodedString.length % 4];
+    return new Buffer(encodedString, 'base64');
+}
+
+function appendZeroByte(bytes) {
+    var result = new Buffer(bytes.length + 1);
+    result[0] = 0;
+    for (var i = 0; i < bytes.length; i++) {
+        result[i + 1] = bytes[i];
+    }
+    return result;
+}
 
 function pkcs8_pad(privateBytes){
   var off1 = new Buffer([Math.floor(privateBytes.length / 256),((privateBytes.length + 22) % 256) ])
@@ -28,6 +45,46 @@ function spki_export(Key){
   return new Buffer(forge.asn1.toDer(forge.pki.publicKeyToAsn1(Key.publicKey)).bytes(),"binary");
 }
 
+function jwk_import(bytes) {
+    if (bytes.d) {
+        // this is a private key, we cannot use it at this moment.
+        throw new Error("NOT IMPLIMENTED"); // sic
+    }
+
+    var SpkiKey = asn.define('SpkiKey', function() {
+        this.seq().obj(
+            this.key('objidSeq').seq().obj(
+                this.key('objid').objid(),
+                this.null_()
+            ),
+            this.key('keydata').bitstr()
+        );
+    });
+
+    var KeySequence = asn.define('KeySequence', function() {
+        this.seq().obj(
+            this.key('n').int(),
+            this.key('e').int()
+        )
+    });
+
+    var keyBytes = KeySequence.encode({
+        n: appendZeroByte(base64urldecode(bytes.n)),
+        e: base64urldecode(bytes.e)
+    }, 'der');
+
+    var publicBytes = SpkiKey.encode({
+        objidSeq: {
+            objid: [1, 2, 840, 113549, 1, 1, 1]//'1.2.840.113549.1.1.1'
+        },
+        keydata: {
+            data: keyBytes
+        }
+    }, 'der');
+
+    return spki_import(publicBytes);
+}
+
 function generate(algorithm){
   var exp = 0, pos = 0
   for (var i = algorithm.publicExponent.length - 1 ; i >= 0 ; i--){
@@ -43,6 +100,7 @@ function RSA(Algorithm){
   var formats = Algorithm.formats
     , spki    = formats.spki
     , pkcs8   = formats.pkcs8
+    , jwk     = formats.jwk
 
 
   // attach common generator
@@ -54,6 +112,8 @@ function RSA(Algorithm){
 
   pkcs8.import = pkcs8_import;
   pkcs8.export = pkcs8_export;
+
+  jwk.import = jwk_import;
 
   return;
 }
